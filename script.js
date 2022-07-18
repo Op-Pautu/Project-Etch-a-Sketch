a52b508dbfe4e5d5c8f97b856ef158d405c883ff
Op Pautu <pautuop@gmail.com>
2022-07-18 12:19:39 +0530

    Tweak button to allow it to reset

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const container = document.querySelector('.container');
 // const btn = document.createElement('button');
 // btn.style.color = 'teal';
 // container.insertBefore(btn, square);
-function createSquares(number) {
+function createSquares(rows, columns) {
+    while (document.querySelector("button") !== null) {
+      document.querySelector("button").remove();
+  }
     
     container.style.overflow = "hidden";
     // container.style.width = "960px";
-    const total = (number * number) + number;
-    const mod = number + 1;
+    const total = (rows * columns) + rows;
+    const mod = rows + 1;
    // yes using grid would be a lot earlier, but one must flex one's flexbox's skills
     for (let i = 1; i < total; i++) {
       const square = document.createElement('div');
@@ -34,7 +37,8 @@ function createSquares(number) {
         })
       }
       }
-      }
+      createButton()
+  }
   
   
   // function getRandomColor() {
@@ -52,10 +56,26 @@ function createSquares(number) {
     resetButton.textContent = "Reset Grid!";
     resetButton.style.margin = "12px";
     buttonDiv.appendChild(resetButton);
-  }
-createButton()
 
-createSquares(16);
+
+
+    resetButton.addEventListener('click', () => {
+      document.querySelectorAll('.boxes').forEach(e => e.remove())
+ 
+      let userGridInput = prompt("Please enter the number of grid squares per side (Max: 100): ");
+      if (userGridInput > 100) {
+          alert("ERROR!  You specified a grid size larger than the max of 100.");
+          return;
+      }
+      rows = userGridInput;
+      columns = userGridInput;
+      createSquares(rows, columns);
+      
+     })
+  }
+
+
+createSquares(16, 16);
 
 
 
